aa7dd77db141e58389db36124e1de21632f246d9
puxiaoruo <1366928318@qq.com>
2020-03-04 13:43:15 +0800

    mint ui 的全部导入和按需导入

--- src/main.js
+++ src/main.js
@@ -1,10 +1,25 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
+import 'mint-ui/lib/style.css'
+
 import Vue from 'vue'
 import App from './App'
 import router from './router'
+// import MintUI from 'mint-ui'
+// 按需导入
+import { Button, Toast } from 'mint-ui'
 
 Vue.config.productionTip = false
+
+Vue.component('mt-button', Button)
+
+/**
+ * 全部导入
+ */
+// Vue.use(MintUI)
+// Vue.prototype.mint = MintUI
+
+Vue.prototype.Toast = Toast
 
 /* eslint-disable no-new */
 new Vue({
